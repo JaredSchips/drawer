37dc8fdd8f3af9e0eebf9069a721d697ace547e5
Jared Schips <110642249+JaredSchips@users.noreply.github.com>
2023-01-11 19:21:31 -0500

    Add image table model

---
+++ models/Image.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+Image.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: true,
+    },
+    public: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "image",
+  }
+);
+
+module.exports = Image;
