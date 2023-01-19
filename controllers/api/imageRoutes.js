085425ccc29ce5e349ff710ffadbfd8b972b6dee
Jared Schips <110642249+JaredSchips@users.noreply.github.com>
2023-01-18 19:16:10 -0500

    replace get all route with api/images/public

--- controllers/api/imageRoutes.js
+++ controllers/api/imageRoutes.js
@@ -2,19 +2,20 @@ const router = require("express").Router();
 const { Image } = require("../../models");
 
 router.get("/public", async (req, res) => {
-    try {
-        const publicImages = await Image.findAll({
-            where: {
-                public: true
-            }
-        });
+  try {
+    const publicImages = await Image.findAll({
+      where: {
+        public: true,
+      },
+    });
 
-        if (!publicImages[0]) res.status(404).json({message: 'No public images found...?'})
+    if (!publicImages[0])
+      res.status(404).json({ message: "No public images found...?" });
 
-        res.status(200).json(publicImages)
-    } catch (err) {
-        res.status(500).json(err)
-    }
+    res.status(200).json(publicImages);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-module.exports = router
+module.exports = router;
