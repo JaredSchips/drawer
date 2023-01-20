68f0ce21b1dc1fc24c2467e058de170cdc3a753c
Jared Schips <110642249+JaredSchips@users.noreply.github.com>
2023-01-20 13:56:53 -0500

    Add signup ability

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -29,7 +29,26 @@ router.post('/login', async (req, res) => {
     });
 
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
