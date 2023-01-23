7868c3f76bb26e8cd43305eee1caa32dae61e493
Jared Schips <110642249+JaredSchips@users.noreply.github.com>
2023-01-23 13:45:05 -0500

    up file drawing file limit to 10mb

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
